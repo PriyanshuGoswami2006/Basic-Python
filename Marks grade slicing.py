e00de615b33cd2cca201bcb7e74f39dbc5cfd2c3
PriyanshuGoswami2006 <priyanshugoswami15@gmail.com>
2026-01-14 13:29:34 +0530

    Create Marks grade slicing.py

---
+++ Marks grade slicing.py
@@ -0,0 +1,17 @@
+num = range(5)
+print (num)
+marks = [33,77,8,65,76,34,2,2,"num","Eren"]
+
+for score in marks :
+    print(score)
+print (marks[5])
+print(marks[3:6])
+
+marks.append(998)
+
+marks.insert(4,"tiger")
+print("Eren" in marks)
+print(len (marks))
+
+from math import sqrt
+print(pow(2, 89))
